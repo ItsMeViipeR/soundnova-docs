99ec48d9b0265c11c41949ba95281b6af2f17470
Nicolas AMATI <skkyz3r.dev@gmail.com>
2023-12-26 11:20:03 +0100

    feat: :tada: Init Nextra project

---
+++ theme.config.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default {
+  logo: <span>SoundNova - Documentation</span>,
+  project: {
+    link: "https://github.com/TallIceCreamCake/CMS-EVENT-WIKI",
+  },
+  i18n: [
+    { locale: "en-US", text: "English" },
+    { locale: "fr", text: "Français" },
+  ],
+};
